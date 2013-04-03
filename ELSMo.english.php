9b01cdbb872a29b0a07a9b16abaf3dc4a27737f3
Andreas Windt <kontakt@andreaswindt.de>
2013-04-03 15:07:50 +0300

    Create ELSMo.english.php

---
+++ ELSMo.english.php
@@ -0,0 +1,8 @@
+<?php
+
+$txt['elsmo_link_pilot'] = 'Link EVE Pilot';
+$txt['elsmo_link_corp'] = 'Link EVE Corporation';
+$txt['elsmo_link_system'] = 'Link EVE Solar System';
+$txt['elsmo_link_station'] = 'Link EVE Station';
+
+?>
